705e3b90b3e2c759c31e76ca248cfb9bce0bb5c5
Adriane Dilao <dilaoaparre@gmail.com>
2023-03-02 21:35:19 +0800

    Non-server Favorites Implementation

--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -1,8 +1,11 @@
 import { Link } from "react-router-dom";
-
 import classes from "./MainNavigation.module.css";
+import { useContext } from "react";
+import FavoritesContext from "../../store/favorites-context";
 
 function MainNavigation() {
+  const favoritesCtx = useContext(FavoritesContext);
+
   return (
     <header className={classes.header}>
       <div className={classes.logo}>👩🏻‍🤝‍🧑🏻 World Meetups</div>
@@ -15,7 +18,12 @@ function MainNavigation() {
             <Link to="/new-meetup">Add New Meetup</Link>
           </li>
           <li>
-            <Link to="/favorites">Favorites</Link>
+            <Link to="/favorites">
+              My Favorites
+              <span className={classes.badge}>
+                {favoritesCtx.totalFavorites}
+              </span>
+            </Link>
           </li>
         </ul>
       </nav>
--- src/components/meetups/MeetupItem.js
+++ src/components/meetups/MeetupItem.js
@@ -1,8 +1,27 @@
 import classes from "./MeetupItem.module.css";
 import Card from "../ui/Card";
 import PropTypes from "prop-types";
+import { useContext } from "react";
+import FavoritesContext from "../../store/favorites-context";
 
 function MeetupItem({ address, description, id, image, key, title }) {
+  const favoritesCtx = useContext(FavoritesContext);
+  const itemIsFavorite = favoritesCtx.itemIsFavorite(id);
+
+  function toggleFavoriteStatusHandler() {
+    if (itemIsFavorite) {
+      favoritesCtx.removeFavorite(id);
+    } else {
+      favoritesCtx.addFavorite({
+        id: id,
+        title: title,
+        description: description,
+        image: image,
+        address: address,
+      });
+    }
+  }
+
   return (
     <li className={classes.item}>
       <Card>
@@ -15,7 +34,9 @@ function MeetupItem({ address, description, id, image, key, title }) {
           <p>{description}</p>
         </div>
         <div className={classes.actions}>
-          <button>To Favorites</button>
+          <button onClick={toggleFavoriteStatusHandler}>
+            {itemIsFavorite ? "Remove From Favorites" : "To Favorites"}
+          </button>
         </div>
       </Card>
     </li>
